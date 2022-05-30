25868b2dea534bde045609088d60193da6144e22
Philippe Maincon <philippe.maincon@sintef.no>
2022-05-30 10:28:45 +0200

    Moved the html documentation to /root/doc for compatibility with GitHub/Pages

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,15 @@ push!(LOAD_PATH,"../src")
 makedocs(sitename ="EspyInsideFunction.jl",
         modules   = [EspyInsideFunction],
         format    = Documenter.HTML(prettyurls = false),
-        pages     = ["index.md"]
+        pages     = ["index.md"],
+        source  = "src",
+        build   = "build"   
         )
 
-deploydocs(repo = "github.com/PhilippeMaincon/EspyInsideFunction.jl.git")
+here = @__DIR__
+mv(here*"\\build\\index.html",here*"\\index.html", force=true)        
+mv(here*"\\build\\search.html",here*"\\search.html", force=true)        
+mv(here*"\\build\\search_index.js",here*"\\search_index.js", force=true)        
+mv(here*"\\build\\assets",here*"\\assets", force=true)        
+#deploydocs(repo = "github.com/PhilippeMaincon/EspyInsideFunction.jl.git")
 
